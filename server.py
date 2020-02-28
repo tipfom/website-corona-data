9c96455b42ebbadbd2dcaf28433eea44947504a6
Tim <tipfom@hotmail.de>
2020-02-28 12:02:11 +0100

    added login token generation

--- server.py
+++ server.py
@@ -1,8 +1,20 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 import json
+from random import randrange
 
 PROTOCOL_VERSION = "0.1"
+
+ALLOWED_TOKEN_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+valid_tokens = []
+
+
+def generateToken(length):
+    token = ""
+    for i in range(length):
+        token += ALLOWED_TOKEN_CHARS[randrange(len(ALLOWED_TOKEN_CHARS))]
+    return token
+
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -10,28 +22,34 @@ class RequestHandler(BaseHTTPRequestHandler):
         post_body = self.rfile.read(content_len)
         post_parsed_content = json.loads(post_body)
         if post_parsed_content["password"] == "defg":
-            self.send_response(200)
+            self.send_response(201)
+            new_token = generateToken(20)
+            valid_tokens.append(new_token)
+            self.send_header('Access-Control-Allow-Credentials', 'true')
+            self.send_header('Access-Control-Allow-Origin',
+                            'http://localhost:4200')
+            self.end_headers()
+            self.wfile.write(("{\"token\":\"" + new_token+"\"}").encode())
         else:
             self.send_response(401)
-            
-        self.send_header('Access-Control-Allow-Credentials', 'true')
-        self.send_header('Access-Control-Allow-Origin', 'http://localhost:4200')
-        self.send_header("Content-type", "text/xml")
-        self.end_headers()
-        
-        #self.wfile.write(json.dumps(response))
+            self.end_headers()
+
+        # self.wfile.write(json.dumps(response))
 
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\n",
-                str(self.path), str(self.headers))
+                     str(self.path), str(self.headers))
 
-    def do_OPTIONS(self):           
-        self.send_response(200)       
-        self.send_header('Access-Control-Allow-Origin', 'http://localhost:4200')
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin',
+                         'http://localhost:4200')
         self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header("Access-Control-Allow-Headers", "x-api-key,Content-Type")
+        self.send_header("Access-Control-Allow-Headers",
+                         "x-api-key,Content-Type")
         self.end_headers()
         logging.info("OPTIONS request,\nPath: %s\nHeaders:\n%s\n\n",
-                str(self.path), str(self.headers))
+                     str(self.path), str(self.headers))
+
 
 def run(server_class=HTTPServer, handler_class=RequestHandler, port=5764):
     logging.basicConfig(level=logging.INFO)
