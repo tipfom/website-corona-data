496db10d553d7ddb7f2d849a4ac0ffee226616e8
Tim <tipfom@hotmail.de>
2020-03-04 22:01:31 +0100

    added ability to retrieve cm data from maria db

--- server.py
+++ server.py
@@ -2,11 +2,25 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 import json
 from random import randrange
+import mysql.connector
+from urllib.parse import urlparse
 
 PROTOCOL_VERSION = "0.1"
 
 ALLOWED_TOKEN_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 valid_tokens = []
+
+mysql_host = 'tim-nas'  # Rechnername (localhost ist dein eigener Rechner)
+mysql_port = 3307
+mysql_user = 'admin'
+mysql_pass = 'YNy2*rJAck%DcS^#'
+mysql_db = 'website_cms'
+
+# Verbindung mit der Datenbank
+mysql_conn = mysql.connector.connect(
+    host=mysql_host, port=mysql_port, user=mysql_user, password=mysql_pass)
+mysql_conn.database = mysql_db
+mysql_cursor = mysql_conn.cursor()
 
 
 def generateToken(length):
@@ -17,6 +31,24 @@ def generateToken(length):
 
 
 class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        if parsed_path.path.startswith("/resource/"):
+            splitted = parsed_path.path.split("/")
+            if len(splitted) == 3:
+                id = splitted[2]
+                sql_query = "SELECT type FROM entries WHERE id=" + id
+                mysql_cursor.execute(sql_query)
+                row = mysql_cursor.fetchone()
+                if row != None:
+                    self.send_response(200)
+                    self.end_headers()
+                    self.wfile.write((row[0] + "\n").encode())
+                    sql_query = "SELECT path FROM resources WHERE ref_entry=" + id
+                    mysql_cursor.execute(sql_query)
+                    for row in mysql_cursor.fetchall():
+                        self.wfile.write((row[0] + "\n").encode())
+
     def do_POST(self):
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len)
@@ -25,12 +57,14 @@ class RequestHandler(BaseHTTPRequestHandler):
             self.send_response(201)
             new_token = generateToken(20)
             valid_tokens.append(new_token)
-            self.send_header('Access-Control-Allow-Origin', 'http://localhost:4200')
+            self.send_header('Access-Control-Allow-Origin',
+                             'http://localhost:4200')
             self.end_headers()
             self.wfile.write(("{\"token\":\"" + new_token+"\"}").encode())
         else:
             self.send_response(401)
-            self.send_header('Access-Control-Allow-Origin', 'http://localhost:4200')
+            self.send_header('Access-Control-Allow-Origin',
+                             'http://localhost:4200')
 
             self.end_headers()
 
@@ -65,3 +99,7 @@ def run(server_class=HTTPServer, handler_class=RequestHandler, port=5764):
 
 
 run()
+
+# close mysql connection
+mysql_cursor.close()
+mysql_conn.disconnect()
