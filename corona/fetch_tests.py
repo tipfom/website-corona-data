8ac51171f78ba08fe0921a1f05f715a43aaf2f54
Tim <tipfom@hotmail.de>
2020-04-04 13:02:57 +0200

    changed fetch tests source

--- corona/fetch_tests.py
+++ corona/fetch_tests.py
@@ -1,58 +1,58 @@
 country_map = {
-    "United States": "US",
-    "Czech Republic": "Czechia",
-    "South Korea": "Korea, South",
+    "World": "global",
+    "USA": "US",
+    "S. Korea": "Korea, South",
+    "UK": "United Kingdom",
     "TOTAL": "global",
 }
 
+
 def try_get_tests():
-    url = "https://ourworldindata.org/grapher/tests-vs-confirmed-cases-covid-19"
+    url = "https://www.worldometers.info/coronavirus/"
 
     from urllib.request import Request, urlopen
-    htmlcontent = urlopen(Request(url, headers={'User-Agent': 'Mozilla/5.0'})).read().decode("utf8")
-    dataurl = "https://ourworldindata.org/" + htmlcontent.split("<link rel=\"preload\" href=\"")[1].split("\"")[0]
-    jsoncontent =  urlopen(Request(dataurl, headers={'User-Agent': 'Mozilla/5.0'})).read().decode("utf8")
-
-    import json
-    json_parsed = json.loads(jsoncontent)
-    country_keys = json_parsed["entityKey"]
-
-    print("reading " + json_parsed["variables"]["142596"]["name"])
-    tests_countries = json_parsed["variables"]["142596"]["entities"]
-    tests_dates = json_parsed["variables"]["142596"]["years"]
-    tests_values = json_parsed["variables"]["142596"]["values"]
-
-    print("reading " + json_parsed["variables"]["142582"]["name"])
-    confirmed_countries = json_parsed["variables"]["142582"]["entities"]
-    confirmed_dates = json_parsed["variables"]["142582"]["years"]
-    confirmed_values = json_parsed["variables"]["142582"]["values"]
-
-    test_data = {}
-
     from datetime import datetime, timedelta
-    for i in range(len(tests_values)):
-        country = country_keys[str(tests_countries[i])]["name"].split(" - ")
-        original_name = country[0]
-        if country_map.__contains__(country[0]):
-            country[0] = country_map[country[0]]
-        country[0] = country[0].replace(" ", "_")
-        
-        confirmed_at_date = 0
-        for k in range(len(confirmed_values)):
-            if country_keys[str(confirmed_countries[k])]["name"].startswith(original_name) and confirmed_dates[k] == tests_dates[i]:
-                confirmed_at_date = confirmed_values[k]
-                break
+    import json
 
-        if len(country) == 2:
-            if not test_data.__contains__(country[0]):
-                test_data.update({country[0]: {"total": 0, "original_name": original_name, "confirmed_cases": confirmed_at_date, "regions": {}, "updated": (datetime(2020,1,21) + timedelta(int(tests_dates[i]))).isoformat()}})
-            test_data[country[0]]["total"] += tests_values[i]
-            test_data[country[0]]["regions"].update({country[1]: tests_values[i]})
-        else:
-            test_data.update({country[0]: {"total": tests_values[i], "original_name": original_name, "confirmed_cases": confirmed_at_date, "updated": (datetime(2020,1,21) + timedelta(int(tests_dates[i]))).isoformat()}})
+    htmlcontent = (
+        urlopen(Request(url, headers={"User-Agent": "Mozilla/5.0"}))
+        .read()
+        .decode("utf8")
+    )
+    table = htmlcontent.split('<table id="main_table_countries_today"')[1].split(
+        "</table>"
+    )[0]
+    tablebody = table.split("<tbody>")[1].split("</tbody>")[0]
+    test_data = {}
+    for row in tablebody.split("</tr>"):
+        columns = row.split("</td>")
+        if len(columns) > 11:
+            try:
+                name = columns[0].split("<a ")[1].split(">")[1].replace("</a", "")
+                country = name
+                if country_map.__contains__(country):
+                    country = country_map[country]
+                country = country.replace(" ", "_")
+                cases = columns[1].split(">")[1]
+                tests = columns[10].split(">")[1]
+                if tests != "":
+                    test_data.update(
+                        {
+                            country: {
+                                "total": tests.replace(",", ""),
+                                "original_name": name,
+                                "confirmed_cases": cases.replace(",", ""),
+                                "updated": datetime.now().isoformat(),
+                            }
+                        }
+                    )
+            except Exception:
+                pass
 
     return json.dumps(test_data).encode()
 
+
 if __name__ == "__main__":
     ret = try_get_tests()
+    print(ret)
     pass
